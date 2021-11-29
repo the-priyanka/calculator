b9aaa2878f1707e5708ad6f23c5f117df24adc36
Priyanka <original.manaria@gmail.com>
2021-11-29 12:29:36 +0530

    added functions - addDecimal, resetAll

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ const clearBtn = document.getElementById("clear-btn");
 function sendNumberValue(number) {
   // if current display value is 0, replace it, if not add number
   const displayValue = calculatorDisplay.textContent;
-  calculatorDisplay.textContent = displayValue === "0"
-    ? number
-    : displayValue + number;
+  calculatorDisplay.textContent =
+    displayValue === "0" ? number : displayValue + number;
+}
+
+function addDecimal() {
+  // if no decimal, add one
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
 }
 
 // add event listeners for numbers, operators, decimal button
@@ -21,6 +27,12 @@ inputBtns.forEach((inputBtn) => {
       sendNumberValue(inputBtn.value)
     );
   } else if (inputBtn.classList.contains("decimal")) {
-    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+    inputBtn.addEventListener("click", addDecimal);
   }
 });
+
+function resetAll() {
+  calculatorDisplay.textContent = "0";
+}
+
+clearBtn.addEventListener("click", resetAll);
