8f9f926c5d37fd71e2b1ee6b38424248dd0aff5b
Priyanka <original.manaria@gmail.com>
2021-11-29 14:50:18 +0530

    operator functionality here

--- script.js
+++ script.js
@@ -2,18 +2,44 @@ const calculatorDisplay = document.querySelector("h1");
 const inputBtns = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear-btn");
 
+let firstValue = 0;
+let operatorValue = "";
+let awaitingNextValue = false;
+
 function sendNumberValue(number) {
-  // if current display value is 0, replace it, if not add number
-  const displayValue = calculatorDisplay.textContent;
-  calculatorDisplay.textContent =
-    displayValue === "0" ? number : displayValue + number;
+  // replace current display value if first value is entered
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    // if current display value is 0, replace it, if not add number
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent =
+      displayValue === "0" ? number : displayValue + number;
+  }
 }
 
 function addDecimal() {
+  // if operator pressed, don't add decimal
+  if (awaitingNextValue) return;
   // if no decimal, add one
   if (!calculatorDisplay.textContent.includes(".")) {
     calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
   }
+}
+
+function useOperator(operator) {
+  const currentValue = Number(calculatorDisplay.textContent);
+  if (!firstValue) {
+    firstValue = currentValue;
+  } else {
+    console.log(currentValue);
+  }
+  // ready for next value, store operator
+  awaitingNextValue = true;
+  operatorValue = operator;
+  console.log(firstValue);
+  console.log(operator);
 }
 
 // add event listeners for numbers, operators, decimal button
@@ -24,14 +50,17 @@ inputBtns.forEach((inputBtn) => {
     );
   } else if (inputBtn.classList.contains("operator")) {
     inputBtn.addEventListener("click", () =>
-      sendNumberValue(inputBtn.value)
+      useOperator(inputBtn.value)
     );
   } else if (inputBtn.classList.contains("decimal")) {
-    inputBtn.addEventListener("click", addDecimal);
+    inputBtn.addEventListener("click", () => addDecimal());
   }
 });
 
 function resetAll() {
+  firstValue = 0;
+  operatorValue = "";
+  awaitingNextValue = false;
   calculatorDisplay.textContent = "0";
 }
 
